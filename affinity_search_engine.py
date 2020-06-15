68399aa62b6551dbc48685da013a2a0227db0fdb
Evgeny Pantyukhin <felester95@gmail.com>
2020-06-15 19:24:44 +0700

    Functions were added to draw the synapse values of two NSs on the canvas during training. A function was added to resize the image when resizing the window.

--- affinity_search_engine.py
+++ affinity_search_engine.py
@@ -1,29 +1,38 @@
-class affinity_analyzer:
-    comparison_order = [[0, 0],
-                        [0, 1],
-                        [1, 0],
-                        [1, 1]]
+class AffinityAnalyzer:
+    _comparison_order = [[0, 0],
+                         [0, 1],
+                         [1, 0],
+                         [1, 1]]
 
-    def __init__(self, synapse_values, indices_elected_neuron):
-        self._top_similar = [[] for i in range(len(self.comparison_order))]
+    def __init__(self):
+        self._top_similar = [[] for i in range(len(self._comparison_order))]
+
+    # Получить списки похожих нейронов
+    def recount_affinity(self, synapse_values, indices_elected_neuron):
+        self._top_similar = [[] for i in range(len(self._comparison_order))]
+
         if len(synapse_values) > 0:
             self._find_top_similar(synapse_values, indices_elected_neuron)
         else:
             print("Инициализация объекта поиска похожих нейронов прошла с ошибкой.")
+        return self._generate_string_response()
 
-    # Находит ТОП схожих с заданными
+    # Находит ТОП 3 схожих с заданными
     def _find_top_similar(self, synapse_values, indices_elected_neuron):
         lists_coincidence = []
-        for i in range(len(self.comparison_order)):
-            lists_coincidence.append(self._neuron_comparison(self.comparison_order[i][0], self.comparison_order[i][1],
+        # Полное сравнение
+        for i in range(len(self._comparison_order)):
+            lists_coincidence.append(self._neuron_comparison(self._comparison_order[i][0], self._comparison_order[i][1],
                                                              indices_elected_neuron[i//2], synapse_values))
 
+        # Топ 3
         for j in range(len(lists_coincidence)):
             for i in range(3):
                 self._top_similar[j].append(
                     [lists_coincidence[j].index(min(lists_coincidence[j])), min(lists_coincidence[j])])
                 lists_coincidence[j][self._top_similar[j][-1][0]] = 1000
 
+    # Вроде лишнее
     def update_synapse(self, synapse_values, indices_elected_neuron):
         if len(synapse_values) > 0:
             self._find_top_similar(synapse_values, indices_elected_neuron)
@@ -35,7 +44,7 @@ class affinity_analyzer:
     def get_top_similar(self):
         return self._generate_string_response()
 
-    # Сравнить указанный нейрон первой сети со всеми нейронами второй. Вернуть список сходства
+    # Сравнить первый нейрон со всеми второй указанной сети.
     def _neuron_comparison(self, index_network_1, index_network_2, index_neuron, synapse_values):
         list_coincidence = []
 
@@ -54,7 +63,7 @@ class affinity_analyzer:
     def _generate_string_response(self):
         top_similar_str = ["" for i in range(len(self._top_similar))]
         for j in range(len(self._top_similar)):
-            top_similar_str[j] += "Самый похожий в сети " + str(self.comparison_order[j][1] + 1) + ": "
+            top_similar_str[j] += "Самый похожий в сети " + str(self._comparison_order[j][1] + 1) + ": "
             for top in self._top_similar[j]:
                 top_similar_str[j] += "Нейрон: " + str(top[0]) + " - отличия: " + str(round(top[1], 1)) + "; "
         return top_similar_str
